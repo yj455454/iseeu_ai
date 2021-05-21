d7b71d83d0d39f3b53aee944fcc29a4491d57fe5
bomi song <bomiii227@gmail.com>
2021-05-21 11:56:11 +0900

    modify .py

--- iseeu_ai.py
+++ iseeu_ai.py
@@ -31,6 +31,10 @@ class IseeU:
     # user 와 unknown 각각 한번씩 실행시켜 줘야 함 (혹은 업데이트 시에 한번 더)
     def make_person_list(self, face_path, target):
         img_paths = self.get_image_path(face_path)
+        # 수정 코드 : Mac - DS.store file 제거 코드
+        if target == 'crop':
+            del img_paths[2]
+            print(img_paths)
 
         for idx, path in enumerate(img_paths):
             img = self.image_processing(path)
@@ -54,23 +58,26 @@ class IseeU:
 
         # 비교 대상이 없을 때 (unknown에 아무도 없을 때를 위한 것)
         if len(person_img) == 0:
-            return False
+            ## 원본 코드
+            # return False
+            ## 수정 코드 : new_unknown_image_record_write() 함수 실행 시킬 수 있는 값 리턴
+            return (target, -1)
 
         self.result_list = []
         
         for num in range(len(person_img)):
             pred_result = []
-
             for img in self.crop_img:
                 pred_result.append(self.model.predict([img, person_img[num]])[0][0])
 
             result = self.make_result(pred_result, target)
             self.result_list.append(result)
             
-        min_value = min(self.result_list, key= lambda x: x[0])
+        min_value = min(self.result_list, key= lambda x: x[0]) # 수정 코드 : min_value는 리스트(result)
         person_id = self.result_list.index(min_value)
 
-        if min_value <= self.threshold:
+        # 수정 코드 : min_value는 리스트이므로 thershold와 비교하는 것은 min_value[0] => 즉 가장 낮은 평균값
+        if min_value[0] <= self.threshold:
             return (target, person_id)
         else: # threshold 를 모두 넘지 못했을 때
             return (target, -1)
@@ -78,7 +85,7 @@ class IseeU:
     def image_record_write(self, target, person_id):
         save_image = cv2.imread(self.result_list[person_id][2])
 
-        file_name = f"record/{target}_{person_id+1}.jpg"
+        file_name = f"/Users/spring/Desktop/record/{target}_{person_id+1}.jpg"
         cv2.imwrite(file_name, save_image)
         # 문제점 : 기록이 겹처서 사진이 덮어쓰기 됨. 시간 정보를 받아서 저장해야 함
         # 그리고 boto3로 클라우드에 올려줘야 함
@@ -87,7 +94,10 @@ class IseeU:
         save_image = cv2.imread(self.crop_dict[0])
 
         file_name_for_face = f"/Users/spring/Desktop/unknown/{len(self.unknown_img)+1}_unknown.jpg"
-        file_name_for_record = f"record/unknown_{len(self.unknown_img)+1}.jpg"
+        print(file_name_for_face)
+        file_name_for_record = f"/Users/spring/Desktop/record/unknown_{len(self.unknown_img)+1}.jpg"
+        print(file_name_for_record)
+        ## 수정 코드 필요 : file_name_for_face 동작이 안됨 !! => unknown 폴더에 이미지 저장 안됨, record는 저장됨
 
         cv2.imwrite(file_name_for_face, save_image)
         cv2.imwrite(file_name_for_record, save_image)
@@ -128,10 +138,14 @@ class IseeU:
 
         pred_min_img_index = pred_list.index(pred_min)  # 유사도 가장 높은 이미지 선택
 
-        if target == 'user':
-            result = [pred_mean, pred_min, self.user_dict[pred_min_img_index]]
-        else: # unknown
-            result = [pred_mean, pred_min, self.unknown_dict[pred_min_img_index]]
+        ## 원본 코드
+        # if target == 'user':
+        #     result = [pred_mean, pred_min, self.crop_dict[pred_min_img_index]]
+        # else: # unknown
+        #     result = [pred_mean, pred_min, self.unknown_dict[pred_min_img_index]]
+        #
+        # return result
 
+        ## 수정 코드
+        result = [pred_mean, pred_min, self.crop_dict[pred_min_img_index]]
         return result
-
